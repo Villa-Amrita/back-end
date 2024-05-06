ad7ccca821f6cb349e448332f16da72edea2c452
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2024-05-06 10:26:55 +0530

    Configure API routes

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,8 +1,18 @@
 import express, { Router } from "express"
 import userRouter from "./user/user.router"
+import roomRouter from "./room/room.router"
+import mealRouter from "./meal/meal.router"
+import mealplanRouter from "./mealplan/mealplan.router"
+import reservationRouter from "./reservation/reservation.router"
+import reservationDailyMealRouter from "./ReservationDailyMeal/reservationDailyMeal.router"
 
 const apiRouter: Router = express.Router()
 
 apiRouter.use("/user", userRouter)
+apiRouter.use("/room", roomRouter)
+apiRouter.use("/meal", mealRouter)
+apiRouter.use("/mealplan", mealplanRouter)
+apiRouter.use("/reservation", reservationRouter)
+apiRouter.use("/reservationDailyMeal", reservationDailyMealRouter)
 
 export default apiRouter
